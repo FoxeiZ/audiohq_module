65807f960cc498433d66982d3eeec27e127507da
FoxeiZ <foxeiz2004@gmail.com>
2022-07-30 23:21:42 +0700

    fix system being unusable in android 12L

--- service_try_enforcing.sh
+++ service_try_enforcing.sh
@@ -6,6 +6,34 @@ mkdir -p /data/misc/audiohq
 
 audiohq --service --force
 
+setup() {
+    if [ -n "`getprop init.svc.audioserver`" ]; then
+        setprop ctl.restart audioserver
+        sleep 1.2
+        if [ "`getprop init.svc.audioserver`" != "running" ]; then
+            # workaround for Android 12 old devices hanging up the audioserver after "setprop ctl.restart audioserver" is executed
+            local pid="`getprop init.svc_debug_pid.audioserver`"
+            if [ -n "$pid" ]; then
+                kill -HUP $pid 1>&2
+            fi
+        fi
+    fi
+
+    elif [ "`getprop ro.system.build.version.release`" -ge "12" ]; then
+        if [ -n "`getprop init.svc.audioserver`" ]; then
+            setprop ctl.restart audioserver
+            sleep 1.2
+            if [ "`getprop init.svc.audioserver`" != "running" ]; then
+                # workaround for Android 12 old devices hanging up the audioserver after "setprop ctl.restart audioserver" is executed
+                local pid="`getprop init.svc_debug_pid.audioserver`"
+                if [ -n "$pid" ]; then
+                    kill -HUP $pid 1>"/dev/null" 2>&1
+                fi
+            fi
+        fi
+    fi
+}
+
 (
-  killall audioserver 2>/dev/null
+    setup 2>/dev/null
 )&
